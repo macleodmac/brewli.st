742172383509f0ebf95a7f394b2aafb670f38057
Jamie MacLeod <macleod.mac@gmail.com>
2023-05-30 22:30:57 +0100

    Increase size of icons

--- src/components/IconBadge/IconBadge.tsx
+++ src/components/IconBadge/IconBadge.tsx
@@ -13,20 +13,28 @@ export function IconBadge({ icon, text, color, ...props }: IconBadgeProps) {
       <Icon as={icon} boxSize={5} mr={1} ml={-1} />
       <TagLabel>{text}</TagLabel>
     </Tag> */}
-      <HStack spacing={0} minHeight="20px" display="flex">
+      <HStack spacing={0} minHeight='20px' display='flex'>
         <Box
           pt={1.5}
           pr={text ? 1 : 2}
           pb={1.5}
           pl={2}
-          height="100%"
-          display="flex"
-          alignItems="center"
+          height='100%'
+          display='flex'
+          alignItems='center'
         >
-          <Icon as={icon} color={color} />
+          <Icon as={icon} color={color} w={5} h={5} />
         </Box>
         {text && (
-          <Box pt={1.5} pr={2} pb={1.5} pl={1} height="100%" display="flex" alignItems="center">
+          <Box
+            pt={1.5}
+            pr={2}
+            pb={1.5}
+            pl={1}
+            height='100%'
+            display='flex'
+            alignItems='center'
+          >
             <Text color={color} fontWeight={'medium'} fontSize={'medium'}>
               {text}
             </Text>
